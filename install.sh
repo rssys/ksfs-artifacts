e9f3007ea5f39b208ef75586b167d6c790e06403
pdlan <pengdinglan@gmail.com>
2026-01-29 01:15:58 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y ./linux-image-6.1.38_6.1.38-13_amd64.deb ./linux-headers-6.1.38_6.1.38-13_amd64.deb
+sudo apt install -y ./linux-image-6.1.38_6.1.38-13_amd64.deb ./linux-headers-6.1.38_6.1.38-13_amd64.deb ./librocksdb7.8_7.8.3-2_amd64.deb
 sudo apt install -y build-essential libfuse3-dev meson ninja-build fio clang autoconf automake \
-    libtool bison flex yacc pkg-config
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly
+    libtool bison flex yacc pkg-config exfatprogs ntfs-3g
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
+  --default-toolchain nightly-2025-08-23 \
+  --target x86_64-unknown-linux-gnu
 . "$HOME/.cargo/env"
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
-wget https://github.com/filebench/filebench/archive/refs/tags/1.4.9.1.tar.gz
-tar xzf 1.4.9.1.tar.gz
-cd filebench-1.4.9.1
+rustup component add rust-src --toolchain nightly-2025-08-23
+git clone https://github.com/filebench/filebench.git
+cd filebench
+git checkout 22620e602cbbebad90c0bd041896ebccf70dbf5f
+git apply < ../filebench.patch
 libtoolize
 aclocal
 autoheader
+automake --add-missing
+libtoolize
 automake --add-missing
 autoconf
 ./configure
